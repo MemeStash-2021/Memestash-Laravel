5b087206019bb2223e1d8afacdf680068a02fee8
Bo <bo.robbrecht@student.howest.be>
2021-03-16 11:17:42 +0100

    Able to reach endpoint

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('users')->group(function () {
+    Route::get(
+        '/',
+        function () {
+            return response('Reached endpoint');
+        }
+    );
+});
