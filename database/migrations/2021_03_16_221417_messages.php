cbdddd2aebdf0c63e7dc0a8b50be6b026ab05c8a
Bo <bo.robbrecht@student.howest.be>
2021-03-16 23:18:48 +0100

    Added migrations for messages

---
+++ database/migrations/2021_03_16_221417_messages.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Messages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table){
+            $table->foreignId('chat_id')->constrained('chats');
+            $table->foreignId('user_id')->constrained('users');
+            $table->dateTime('timestamp');
+            $table->text('message');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
