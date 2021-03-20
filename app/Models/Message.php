5f8a7a1090ee85a12a3a14ef4b67a2bdbc8db96b
Bo <bo.robbrecht@student.howest.be>
2021-03-20 10:16:23 +0100

    Added relationships & fillable fields for Message model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'chat_id',
+        'user_id',
+        'timestamp',
+        'message'
+    ];
+
+    public function chat(){
+        return $this->belongsTo(Chat::class, 'chat_id');
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
 }
