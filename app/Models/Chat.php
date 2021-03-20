075f1738a6b7a1d870470b03f1be721ff3bc046c
Bo <bo.robbrecht@student.howest.be>
2021-03-20 10:09:54 +0100

    Added relationships & fillable fields for Chat model

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Chat extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'participant_id_1',
+        'participant_id_2'
+    ];
+
+    public function participant_1(){
+        return $this->belongsTo(User::class, 'participant_1_id');
+    }
+
+    public function participant_2(){
+        return $this->belongsTo(User::class, 'participant_2_id');
+    }
 }
