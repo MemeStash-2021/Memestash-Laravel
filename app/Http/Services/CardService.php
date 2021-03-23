1bfa4df7f4f2fb34e2dd3bce311a3c59e686db94
Bo <bo.robbrecht@student.howest.be>
2021-03-23 16:49:39 +0100

    Able to return a card, queried by it's id

--- app/Http/Services/CardService.php
+++ app/Http/Services/CardService.php
@@ -16,21 +16,13 @@ class CardService
         $this -> model = $card;
     }
 
-    public function getCards()
+    public function getCards(Request $request)
     {
-        //TODO: When implementing actual call, make sure that it also can be filtered on id & name
-        $res = array();
-        for($i = 0; $i < 8; $i++){
-            array_push($res, [
-                "id" => $i,
-                    "name" => "Card".$i,
-                    "image" => "https://via.placeholder.com/640x480.png/0044bb?text=ipsum",
-                    "description" => "blah",
-                    "cost" => 800
-                ]
-            );
+        $name = $request->input('name');
+        $id = $request->input('id');
+        if($id !== null){
+            return Card::find($id);
         }
-        return json_encode($res);
     }
 
     public function getUserCards($id){
