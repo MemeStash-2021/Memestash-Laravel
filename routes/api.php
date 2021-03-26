69564d0cf33ecb1ca6e01fe2c5063694068a9133
Bo <bo.robbrecht@student.howest.be>
2021-03-26 09:30:13 +0100

    Re-indexed routes into separate groups to avoid 1 big routing group

--- routes/api.php
+++ routes/api.php
@@ -22,20 +22,32 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/*User endpoints*/
 Route::prefix('users')->group(function () {
     Route::get('/',[UserController::class, 'index']);
-    Route::get('/{ouid}', [UserController::class, 'show']);
     Route::put('/', [UserController::class, 'store']);
-    Route::patch('/{ouid}', [UserController::class, 'update']);
-    Route::get('/{ouid}/cards', [CardController::class, 'show']);
-    Route::put('/{ouid}/cards/{cid}', [CardController::class, 'store']);
-    Route::put('/{ouid}/wallet', [WalletController::class, 'add']);
-    Route::get('/{ouid}/chats', [ChatController::class, 'index']);
-    Route::get('/{ouid}/chats/{tuid}', [ChatController::class, 'show']);
-    Route::patch('/{ouid}/chats/{tuid}', [ChatController::class, 'update']);
-    Route::put('/{ouid}/chats/{tuid}', [ChatController::class, 'store']);
 });
 
+Route::prefix('users/{ouid}')->group(function () {
+    Route::get('/', [UserController::class, 'show']);
+    Route::patch('/', [UserController::class, 'update']);
+    Route::put('/wallet', [WalletController::class, 'add']);
+    Route::get('/chats', [ChatController::class, 'index']);
+});
+
+/*Cards endpoints*/
 Route::prefix('cards') -> group(function (){
     Route::get('/', [CardController::class, 'index']);
+});
+
+Route::prefix('users/{ouid}/cards')->group(function (){
+    Route::get('/', [CardController::class, 'show']);
+    Route::put('/{cid}', [CardController::class, 'store']);
+});
+
+/*Chat endpoints*/
+Route::prefix('users/{ouid}/chats/{tuid}')->group(function (){
+    Route::get('/', [ChatController::class, 'show']);
+    Route::patch('/', [ChatController::class, 'update']);
+    Route::put('/', [ChatController::class, 'store']);
 });
